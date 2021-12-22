0e4c724f70d40bdc3f3a65ebb6d2808e3bbb8959
McPenquen <majda.calk@outlook.com>
2021-12-22 16:37:09 +0100

    Add body rotation by mouse

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -13,7 +13,9 @@ public class Player : MonoBehaviour
 
     [Header("Object References")]
     [SerializeField] private GameObject cam = null; // camera
+    [SerializeField] private GameObject body = null; // body    
     private Rigidbody rb = null;
+    private Animator animator = null;
     
     // The player is can jump - is touching the floor/and object with feet
     private bool canJump = false;
@@ -22,8 +24,14 @@ public class Player : MonoBehaviour
     {
         // Save the cam reference
         cam = transform.GetChild(0).gameObject;
+        // Save the cam reference
+        body = transform.GetChild(2).gameObject;
+
         // Save rigid body reference
         rb = GetComponent<Rigidbody>();
+        // Save animator reference
+        animator = body.GetComponent<Animator>();
+
         // Save the perspective
         is1stPP = GameManager.GetIs1stPP();
     }
@@ -44,25 +52,42 @@ public class Player : MonoBehaviour
         float mouseYInput = Input.GetAxis("Mouse Y");
         float jumpInput = Input.GetAxis("Jump");
 
-        // Adjust the forward and side vectors to 2D
-        Vector3 twoDForward = cam.transform.forward;
-        Vector3 twoDSide = Vector3.Cross(cam.transform.forward, transform.up);
-        twoDForward.y = 0;
-        twoDSide.y = 0;
+        // If movement is detected
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            // Adjust the forward and side vectors to 2D
+            Vector3 twoDForward = cam.transform.forward;
+            Vector3 twoDSide = Vector3.Cross(cam.transform.forward, transform.up);
+            twoDForward.y = 0;
+            twoDSide.y = 0;
 
-        // Get the new position
-        Vector3 newPosition = transform.position 
-            + twoDForward.normalized * movementSpeed * Time.deltaTime * verticalInput
-            + - twoDSide.normalized * movementSpeed * Time.deltaTime * horizontalInput;
+            // Get the new position
+            Vector3 newPosition = transform.position 
+                + twoDForward.normalized * movementSpeed * Time.deltaTime * verticalInput
+                + - twoDSide.normalized * movementSpeed * Time.deltaTime * horizontalInput;
 
-        // Move the object to the new position
-        //rb.MovePosition(newPosition);
-        transform.position = newPosition;    
+            // Move the object to the new position
+            //rb.MovePosition(newPosition);
+            transform.position = newPosition;
 
+            // Set walking animation to false
+            animator.SetBool("isWalking", false); 
+        }
+        else
+        {
+            // Set walking animation to false
+            animator.SetBool("isWalking", false);
+        }
+
+        // Rotate the camera
         cam.transform.Rotate(-mouseYInput * rotationSpeed, mouseXInput * rotationSpeed, 0);
         // Freeze the z-axis rotation
         Vector3 currentAngles = cam.transform.eulerAngles;
-        cam.transform.eulerAngles = new Vector3(currentAngles.x, currentAngles.y, 0); 
+        cam.transform.eulerAngles = new Vector3(currentAngles.x, currentAngles.y, 0);
+        // Rotate the body
+        body.transform.Rotate(0, mouseXInput * rotationSpeed, 0);
+        // Freeze the z-axis & x-axis rotation
+        body.transform.eulerAngles = new Vector3(0, currentAngles.y, 0);
     
         // Detect jumping
         if (jumpInput != 0 && canJump)
